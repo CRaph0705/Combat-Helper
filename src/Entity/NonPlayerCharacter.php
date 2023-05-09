8eb7d39d280eeee75b5f65e29dcac2d0f15fbd93
CRaph0705 <raphaelcochrane@hotmail.fr>
2023-05-09 17:27:17 +0200

    npc entity added

---
+++ src/Entity/NonPlayerCharacter.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NonPlayerCharacterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NonPlayerCharacterRepository::class)]
+class NonPlayerCharacter
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $HP = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $AC = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $initiative = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getHP(): ?int
+    {
+        return $this->HP;
+    }
+
+    public function setHP(?int $HP): self
+    {
+        $this->HP = $HP;
+
+        return $this;
+    }
+
+    public function getAC(): ?int
+    {
+        return $this->AC;
+    }
+
+    public function setAC(?int $AC): self
+    {
+        $this->AC = $AC;
+
+        return $this;
+    }
+
+    public function getInitiative(): ?int
+    {
+        return $this->initiative;
+    }
+
+    public function setInitiative(?int $initiative): self
+    {
+        $this->initiative = $initiative;
+
+        return $this;
+    }
+}
