46edf8f7949c8879b6644fbe1d29a98a78417ecb
CRaph0705 <raphaelcochrane@hotmail.fr>
2023-05-12 17:08:58 +0200

    front landing page : parchment css js

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,22 @@ import './styles/app.scss';
 import './bootstrap';
 
 console.log('toto');
+
+
+// First call to define "parchment" height
+document.onload = ScrollHeight();
+
+// Redraw when viewport is modified
+window.addEventListener('resize', function(event){
+  ScrollHeight();
+});
+
+
+function ScrollHeight() {
+  var content = document.querySelector('#parchment');
+  var container = document.querySelector('#contain');
+
+  // SVG feTurbulence can modify all others elements, fo this reason "parchment" is in another <div> and in absolute position.
+  // so for a better effect, absolute height is defined by his content.
+  content.style.height = container.offsetHeight + 'px';
+}
