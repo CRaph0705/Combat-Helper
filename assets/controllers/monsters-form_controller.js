a03ae069dcf5eb8479aad5829091112d5d14638a
CRaph0705 <raphaelcochrane@hotmail.fr>
2023-08-25 15:22:14 +0200

    add a monster link changed for a button, problem solved

--- assets/controllers/monsters-form_controller.js
+++ assets/controllers/monsters-form_controller.js
@@ -27,9 +27,8 @@ export default class extends Controller {
                 addTagFormDeleteLink(monster);
             });
 
-            const addMonsterTagLink = document.createElement('a');
+            const addMonsterTagLink = document.createElement('button');
             addMonsterTagLink.classList.add('add_tag_list');
-            addMonsterTagLink.href = '#';
             addMonsterTagLink.innerText = 'Ajouter un monstre';
             addMonsterTagLink.dataset.collectionHolderClass = 'monsters-content';
 
@@ -51,9 +50,13 @@ export default class extends Controller {
                 collectionHolder.dataset.index++;
 
 
+                // add a delete link to the new form
                 addTagFormDeleteLink(item);
-
+                // ajust the height of the page with the new form
                 ScrollHeight();
+                // scroll the page to the new form
+                item.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                console.log("scrollIntoView");
             };
 
 
