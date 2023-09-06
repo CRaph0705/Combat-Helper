f556069cd2dd591372da50570f883086237f3ff3
CRaph0705 <raphaelcochrane@hotmail.fr>
2023-09-06 23:48:33 +0200

    fixtures updated, player schema updated, player index, form, show, edit css added

--- src/Entity/PlayerCharacter.php
+++ src/Entity/PlayerCharacter.php
@@ -27,8 +27,32 @@ class PlayerCharacter
     #[ORM\Column(nullable: true)]
     private ?int $initiative = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $speed = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $strength = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $dexterity = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $constitution = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $intelligence = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $wisdom = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $charisma = null;
+
     #[ORM\OneToMany(mappedBy: 'playerCharacter', targetEntity: EncounterPlayerCharacter::class, orphanRemoval: true)]
     private Collection $encounterPlayerCharacters;
+
+    #[ORM\Column]
+    private ?int $level = null;
 
     public function __construct()
     {
@@ -93,6 +117,91 @@ class PlayerCharacter
         return $this;
     }
 
+
+    public function getSpeed(): ?string
+    {
+        return $this->speed;
+    }
+
+    public function setSpeed(?string $speed): self
+    {
+        $this->speed = $speed;
+
+        return $this;
+    }
+
+    public function getStrength(): ?int
+    {
+        return $this->strength;
+    }
+
+    public function setStrength(?int $strength): self
+    {
+        $this->strength = $strength;
+
+        return $this;
+    }
+
+    public function getDexterity(): ?int
+    {
+        return $this->dexterity;
+    }
+
+    public function setDexterity(?int $dexterity): self
+    {
+        $this->dexterity = $dexterity;
+
+        return $this;
+    }
+
+    public function getConstitution(): ?int
+    {
+        return $this->constitution;
+    }
+
+    public function setConstitution(?int $constitution): self
+    {
+        $this->constitution = $constitution;
+
+        return $this;
+    }
+
+    public function getIntelligence(): ?int
+    {
+        return $this->intelligence;
+    }
+
+    public function setIntelligence(?int $intelligence): self
+    {
+        $this->intelligence = $intelligence;
+
+        return $this;
+    }
+
+    public function getWisdom(): ?int
+    {
+        return $this->wisdom;
+    }
+
+    public function setWisdom(?int $wisdom): self
+    {
+        $this->wisdom = $wisdom;
+
+        return $this;
+    }
+
+    public function getCharisma(): ?int
+    {
+        return $this->charisma;
+    }
+
+    public function setCharisma(?int $charisma): self
+    {
+        $this->charisma = $charisma;
+
+        return $this;
+    }
+
     /**
      * @return Collection<int, EncounterPlayerCharacter>
      */
@@ -119,6 +228,18 @@ class PlayerCharacter
                 $encounterPlayerCharacter->setPlayerCharacter(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): self
+    {
+        $this->level = $level;
 
         return $this;
     }
--- src/Form/PlayerCharacterType.php
+++ src/Form/PlayerCharacterType.php
@@ -6,6 +6,7 @@ use App\Entity\PlayerCharacter;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,15 +16,90 @@ class PlayerCharacterType extends AbstractType
         {
     
             $builder
-                ->add('name')
-                ->add('hp')
-                ->add('ac', IntegerType::class, [
-                    'required' => false,
-                    'label' => 'Classe d\'armure (AC)',
-                    'attr' => [
-                        'placeholder' => 'Classe d\'armure (AC)',
-                    ],
-                ])
+            ->add('name', TextType::class, [
+                'label' => 'Name',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('hp' , IntegerType::class, [
+                'label' => 'HP',
+                'attr' => [
+                    'min' => 0,
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('ac', IntegerType::class, [
+                'required' => false,
+                'label' => 'AC',
+                'attr' => [
+                    'min' => 0,
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('strength', IntegerType::class, [
+                'required' => false,
+                'label' => 'STR',
+                'attr' => [
+                    'min' => 0,
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('dexterity', IntegerType::class, [
+                'required' => false,
+                'label' => 'DEX',
+                'attr' => [
+                    'min' => 0,
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('constitution', IntegerType::class, [
+                'required' => false,
+                'label' => 'CON',
+                'attr' => [
+                    'min' => 0,
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('intelligence', IntegerType::class, [
+                'required' => false,
+                'label' => 'INT',
+                'attr' => [
+                    'min' => 0,
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('wisdom', IntegerType::class, [
+                'required' => false,
+                'label' => 'WIS',
+                'attr' => [
+                    'min' => 0,
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('charisma', IntegerType::class, [
+                'required' => false,
+                'label' => 'CHA',
+                'attr' => [
+                    'min' => 0,
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('level', IntegerType::class, [
+                'required' => false,
+                'label' => 'LVL',
+                'attr' => [
+                    'min' => 1,
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('speed', TextType::class, [
+                'required' => false,
+                'label' => 'SPD',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
         ;
         $playerCharacter = $options['data'] ?? null;
 
