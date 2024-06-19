b5413fbe87db8d15ac5bf433647dc7531a9b789e
Raphael Cochrane <raphael.cochrane94@gmail.com>
2024-06-19 20:46:57 +0200

    monster entity : speed changed for all different speed types, fixtures commented

--- src/DataFixtures/MonsterFixtures.php
+++ src/DataFixtures/MonsterFixtures.php
@@ -14,7 +14,7 @@ class MonsterFixtures extends Fixture
         $rat->setName('Rat');
         $rat->setHp(1);
         $rat->setAc(10);
-        $rat->setSpeed('6 m');
+        // $rat->setSpeed('6 m');
         $rat->setStrength(2);
         $rat->setDexterity(11);
         $rat->setConstitution(9);
@@ -28,7 +28,7 @@ class MonsterFixtures extends Fixture
         $goblin->setName('Gobelin');
         $goblin->setHp(7);
         $goblin->setAc(15);
-        $goblin->setSpeed('9 m');
+        // $goblin->setSpeed('9 m');
         $goblin->setStrength(8);
         $goblin->setDexterity(14);
         $goblin->setConstitution(10);
@@ -42,7 +42,7 @@ class MonsterFixtures extends Fixture
         $wolf->setName('Loup');
         $wolf->setHp(11);
         $wolf->setAc(13);
-        $wolf->setSpeed('12 m');
+        // $wolf->setSpeed('12 m');
         $wolf->setStrength(12);
         $wolf->setDexterity(15);
         $wolf->setConstitution(12);
@@ -56,7 +56,7 @@ class MonsterFixtures extends Fixture
         $giantSpider->setName('Araignée géante');
         $giantSpider->setHp(26);
         $giantSpider->setAc(14);
-        $giantSpider->setSpeed('9 m, escalade 9 m');
+        // $giantSpider->setSpeed('9 m, escalade 9 m');
         $giantSpider->setStrength(14);
         $giantSpider->setDexterity(16);
         $giantSpider->setConstitution(12);
@@ -70,7 +70,7 @@ class MonsterFixtures extends Fixture
         $pseudodragon->setName('Pseudodragon');
         $pseudodragon->setHp(7);
         $pseudodragon->setAc(13);
-        $pseudodragon->setSpeed('4,50 m, vol 18 m');
+        // $pseudodragon->setSpeed('4,50 m, vol 18 m');
         $pseudodragon->setStrength(6);
         $pseudodragon->setDexterity(15);
         $pseudodragon->setConstitution(13);
@@ -84,7 +84,7 @@ class MonsterFixtures extends Fixture
         $troll->setName('Troll');
         $troll->setHp(84);
         $troll->setAc(15);
-        $troll->setSpeed('9 m');
+        // $troll->setSpeed('9 m');
         $troll->setStrength(18);
         $troll->setDexterity(13);
         $troll->setConstitution(20);
@@ -98,7 +98,7 @@ class MonsterFixtures extends Fixture
         $fireGiant->setName('Géant du feu');
         $fireGiant->setHp(162);
         $fireGiant->setAc(18);
-        $fireGiant->setSpeed('9 m');
+        // $fireGiant->setSpeed('9 m');
         $fireGiant->setStrength(25);
         $fireGiant->setDexterity(9);
         $fireGiant->setConstitution(23);
@@ -112,7 +112,7 @@ class MonsterFixtures extends Fixture
         $shadowDemon->setName('Démon des ombres');
         $shadowDemon->setHp(66);
         $shadowDemon->setAc(13);
-        $shadowDemon->setSpeed('9 m, vol 9m');
+        // $shadowDemon->setSpeed('9 m, vol 9m');
         $shadowDemon->setStrength(1);
         $shadowDemon->setDexterity(17);
         $shadowDemon->setConstitution(12);
@@ -126,7 +126,7 @@ class MonsterFixtures extends Fixture
         $ghost->setName('Fantôme');
         $ghost->setHp(45);
         $ghost->setAc(11);
-        $ghost->setSpeed('0 m, vol 12m stationnaire');
+        // $ghost->setSpeed('0 m, vol 12m stationnaire');
         $ghost->setStrength(7);
         $ghost->setDexterity(13);
         $ghost->setConstitution(10);
@@ -140,7 +140,7 @@ class MonsterFixtures extends Fixture
         $zombie->setName('Zombi');
         $zombie->setHp(22);
         $zombie->setAc(8);
-        $zombie->setSpeed('6 m');
+        // $zombie->setSpeed('6 m');
         $zombie->setStrength(13);
         $zombie->setDexterity(6);
         $zombie->setConstitution(16);
@@ -154,7 +154,7 @@ class MonsterFixtures extends Fixture
         $skeleton->setName('Squelette');
         $skeleton->setHp(13);
         $skeleton->setAc(13);
-        $skeleton->setSpeed('9 m');
+        // $skeleton->setSpeed('9 m');
         $skeleton->setStrength(10);
         $skeleton->setDexterity(14);
         $skeleton->setConstitution(15);
@@ -168,7 +168,7 @@ class MonsterFixtures extends Fixture
         $vampire->setName('Vampire');
         $vampire->setHp(144);
         $vampire->setAc(16);
-        $vampire->setSpeed('9 m');
+        // $vampire->setSpeed('9 m');
         $vampire->setStrength(18);
         $vampire->setDexterity(18);
         $vampire->setConstitution(18);
@@ -182,7 +182,7 @@ class MonsterFixtures extends Fixture
         $lich->setName('Liche');
         $lich->setHp(135);
         $lich->setAc(17);
-        $lich->setSpeed('9 m');
+        // $lich->setSpeed('9 m');
         $lich->setStrength(11);
         $lich->setDexterity(16);
         $lich->setConstitution(16);
