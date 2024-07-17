ebc655e448a8dd0c2dac7360f5de8f465021e562
Raphael Cochrane <raphael.cochrane94@gmail.com>
2024-07-17 19:23:17 +0200

    searchbar added in monster index, turbo frame updated after filter

--- assets/controllers/monster-index_controller.js
+++ assets/controllers/monster-index_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
+    staticMonsters = [];
 
     connect() {
         // console.log("monster-index controller connected");
@@ -11,6 +12,8 @@ export default class extends Controller {
         }
 
         const monsters = document.querySelectorAll(".monster");
+        this.staticMonsters = Array.from(monsters);
+        // console.log(this.staticMonsters);
         let currentMonsterId = null;
         monsters[0].classList.add("unit-selected");
         turboFrame.src = monsters[0].dataset.src;
@@ -29,5 +32,42 @@ export default class extends Controller {
 
             });
         });
+    }
+
+    filterMonsters() {
+        // console.log("filterMonsters");
+        const searchInput = document.getElementById("monsterSearch");
+        // console.log(searchInput.value);
+        const monsters = document.querySelectorAll(".monster");
+
+        const searchValue = searchInput.value.toLowerCase();
+
+        monsters.forEach((monster) => {
+            const monsterName = monster.dataset.name.toLowerCase();
+            const shouldDisplay = monsterName.includes(searchValue);
+
+            if (shouldDisplay) {
+                monster.style.display = "block";
+            } else {
+                monster.style.display = "none";
+            }
+        });
+
+        let firstVisibleMonster = null;
+        const displayedMonsters = document.querySelectorAll(".monster");
+        for (let i = 0; i < displayedMonsters.length; i++) {
+            if (displayedMonsters[i].style.display !== "none") {
+                firstVisibleMonster = displayedMonsters[i];
+                break;
+            }
+        }
+        if (firstVisibleMonster) {
+            const turboFrame = document.querySelector("turbo-frame");
+            turboFrame.src = firstVisibleMonster.dataset.src;
+
+            monsters.forEach((m) => m.classList.remove("unit-selected"));
+            firstVisibleMonster.classList.add("unit-selected");
+        }
+
     }
 }
